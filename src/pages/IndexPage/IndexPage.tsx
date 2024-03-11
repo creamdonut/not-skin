b82f3064a5c7ac05420c01481f7b50a7f618836d
Sergey Chikirev <mrretoy@gmail.com>
2024-03-12 00:47:29 +0400

    try

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -5,10 +5,17 @@ import type { FC } from 'react';
 import { Link } from '../../components/Link';
 import { Page } from '../../components/Page';
 import { routes } from '../../navigation';
+import { useInitData } from '@tma.js/sdk-react';
 
 export const IndexPage: FC = () => {
+  const initData = useInitData();
+
   return (
-    <Page title='Home Page 123'>
+    <Page title='Home Page '>
+      <p>
+        Welcome to the TMA.js SDK React boilerplate, {initData?.user?.firstName}{' '}
+        {initData?.user?.lastName}!
+      </p>
       <p>
         This page is a home page in this boilerplate. You can use the links
         below to visit other pages with their own functionality.
